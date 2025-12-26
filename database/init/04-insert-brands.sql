75d76514d91469c1ef28b8397165224cf5446e2a
hyunsoo98 <hs980111@naver.com>
2025-12-26 20:45:02 +0900

    Add files via upload

---
+++ database/init/04-insert-brands.sql
@@ -0,0 +1,226 @@
+-- ============================================================
+
+-- Brands Data Migration (from 2512252207.jsonl)
+
+-- ============================================================
+
+
+INSERT INTO brands (name)
+VALUES ('JMW')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('글램팜')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('놋담')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('라 리베라')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('라네즈')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('라보에이치')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('래핑찰리')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('려')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('롱테이크')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('마몽드')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('메디니스')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('메디안')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('메디힐리')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('메이크온')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('미쟝센')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('바이탈뷰티')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('보아르')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('보타닉센스')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('비레디')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('설화수')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('세잔느')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('아모레베이직')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('아모레성수')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('아모레퍼시픽')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('아모스 프로페셔널')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('아윤채')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('아이오페')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('앞바다즈')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('언노운코드')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('에뛰드')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('에스쁘아')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('에스트라')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('에이피뷰티')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('오딧세이')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('오설록')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('오아')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('온호프')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('울트라잇츠')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('유행화장')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('이니스프리')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('이오시카')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('일리윤')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('정글몬스터')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('제니퍼룸')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('코스알엑스')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('쾌발')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('퍼즐우드')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('풋케어랩')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('프리메라')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('피카소')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('피카소 꼴레지오니')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('한율')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('해피바스')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('헤라')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO brands (name)
+VALUES ('홀리추얼')
+ON CONFLICT (name) DO NOTHING;
