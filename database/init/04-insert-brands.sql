b3e9ee367a25de3b4af731707ba9e56d903d8ea5
hyunsoo98 <hs980111@naver.com>
2025-12-26 20:40:19 +0900

    Update 04-insert-brands.sql

--- database/init/04-insert-brands.sql
+++ database/init/04-insert-brands.sql
@@ -5,442 +5,222 @@
 -- ============================================================
 
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    'JMW',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('JMW')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '글램팜',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('글램팜')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '놋담',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('놋담')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '라 리베라',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('라 리베라')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '라네즈',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('라네즈')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '라보에이치',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('라보에이치')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '래핑찰리',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('래핑찰리')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '려',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('려')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '롱테이크',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('롱테이크')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '마몽드',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('마몽드')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '메디니스',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('메디니스')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '메디안',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('메디안')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '메디힐리',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('메디힐리')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '메이크온',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('메이크온')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '미쟝센',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('미쟝센')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '바이탈뷰티',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('바이탈뷰티')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '보아르',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('보아르')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '보타닉센스',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('보타닉센스')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '비레디',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('비레디')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '설화수',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('설화수')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '세잔느',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('세잔느')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '아모레베이직',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('아모레베이직')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '아모레성수',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('아모레성수')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '아모레퍼시픽',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('아모레퍼시픽')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '아모스 프로페셔널',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('아모스 프로페셔널')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '아윤채',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('아윤채')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '아이오페',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('아이오페')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '앞바다즈',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('앞바다즈')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '언노운코드',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('언노운코드')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '에뛰드',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('에뛰드')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '에스쁘아',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('에스쁘아')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '에스트라',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('에스트라')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '에이피뷰티',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('에이피뷰티')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '오딧세이',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('오딧세이')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '오설록',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('오설록')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '오아',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('오아')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '온호프',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('온호프')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '울트라잇츠',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('울트라잇츠')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '유행화장',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('유행화장')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '이니스프리',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('이니스프리')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '이오시카',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('이오시카')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '일리윤',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('일리윤')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '정글몬스터',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('정글몬스터')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '제니퍼룸',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('제니퍼룸')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '코스알엑스',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('코스알엑스')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '쾌발',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('쾌발')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '퍼즐우드',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('퍼즐우드')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '풋케어랩',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('풋케어랩')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '프리메라',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('프리메라')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '피카소',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('피카소')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '피카소 꼴레지오니',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('피카소 꼴레지오니')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '한율',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('한율')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '해피바스',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('해피바스')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '헤라',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('헤라')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO brands (name, created_at, updated_at)
-VALUES (
-    '홀리추얼',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+INSERT INTO brands (name)
+VALUES ('홀리추얼')
 ON CONFLICT (name) DO NOTHING;
